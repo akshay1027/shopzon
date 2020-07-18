3a421bef1fd7eab2342a1edbf9a00ac1cc31ae49
akshay1027 <65683151+akshay1027@users.noreply.github.com>
2020-07-18 22:01:27 +0530

    init

--- shopzon/src/App.js
+++ shopzon/src/App.js
@@ -48,35 +48,35 @@ function App() {
           
           <Product className="product1" 
                    image="/images/im1.jpg" al="pant" name="Levis" desc="Men's fit denim pant" 
-                   rating="4.5 stars(15 reviews)" price="Rs.999" />
+                   rating="4.5 stars(15 reviews)" price="Rs.2999" />
 
           <Product className="product1" 
-                   image="/images/im1.jpg" al="pant" name="Levis" desc="Men's fit denim pant" 
-                   rating="4.5 stars(15 reviews)" price="Rs.999" />
+                   image="/images/im5.jpg" al="pant" name="Levis" desc="Men's fit pant" 
+                   rating="4.7 stars(15 reviews)" price="Rs.1999" />
 
           <Product className="product1" 
-                   image="/images/im1.jpg" al="pant" name="Levis" desc="Men's fit denim pant" 
-                   rating="4.5 stars(15 reviews)" price="Rs.999" />
+                   image="/images/img6.jpg" al="pant" name="Levis" desc="Men's fit trousers" 
+                   rating="4.3 stars(15 reviews)" price="Rs.599" />
 
           <Product className="product1" 
-                   image="/images/im1.jpg" al="pant" name="Levis" desc="Men's fit denim pant" 
-                   rating="4.5 stars(15 reviews)" price="Rs.999" /> 
+                   image="/images/im2.jpg" al="pant" name="Levis" desc="Men's fit jeans pant" 
+                   rating="3.5 stars(15 reviews)" price="Rs.899" /> 
 
           <Product className="product1" 
-                   image="/images/im1.jpg" al="pant" name="Levis" desc="Men's fit denim pant" 
-                   rating="4.5 stars(15 reviews)" price="Rs.999" />
+                   image="/images/im3.jpg" al="pant" name="Levis" desc="Men's fit denim pant" 
+                   rating="4.8 stars(15 reviews)" price="Rs.2999" />
 
           <Product className="product1" 
-                   image="/images/im1.jpg" al="pant" name="Levis" desc="Men's fit denim pant" 
-                   rating="4.5 stars(15 reviews)" price="Rs.999" />
+                   image="/images/im4.jpg" al="pant" name="Levis" desc="Men's fit denim pant" 
+                   rating="4.9 stars(15 reviews)" price="Rs.559" />
 
           <Product className="product1" 
-                   image="/images/im1.jpg" al="pant" name="Levis" desc="Men's fit denim pant" 
-                   rating="4.5 stars(15 reviews)" price="Rs.999" />
+                   image="/images/im5.jpg" al="pant" name="Levis" desc="Men's fit denim pant" 
+                   rating="4.1 stars(15 reviews)" price="Rs.1349" />
 
           <Product className="product1" 
-                   image="/images/im1.jpg" al="pant" name="Levis" desc="Men's fit denim pant" 
-                   rating="4.5 stars(15 reviews)" price="Rs.999" /> 
+                   image="/images/img6.jpg" al="pant" name="Levis" desc="Men's fit denim pant" 
+                   rating="4.7 stars(15 reviews)" price="Rs.999" /> 
       </main>
 
       <footer>All Rights Reserved</footer>
